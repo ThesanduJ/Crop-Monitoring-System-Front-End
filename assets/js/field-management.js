8ee870fcbf88cc2c14c87c6fbb6493a8ff8a91ad
ThesanduJ <thesandu1602500@gmail.com>
2024-11-22 23:13:10 +0530

    add functions to pickup current location

---
+++ assets/js/field-management.js
@@ -0,0 +1,38 @@
+
+const parent = document.querySelector("#field-location");
+const x = parent.children[0];
+
+// const x = document.getElementById("field-location");
+
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.value = "Latitude: " + position.coords.latitude +
+        " Longitude: " + position.coords.longitude;
+    // x.classList.add("is-valid");
+}
+
+function showError(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            x.innerHTML = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            x.innerHTML = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            x.innerHTML = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            x.innerHTML = "An unknown error occurred."
+            break;
+    }
+}
+
