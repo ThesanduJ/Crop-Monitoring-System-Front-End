d57e042a2b00a25fc00305f6950c55a8638d86c6
ThesanduJ <thesandu1602500@gmail.com>
2024-11-24 22:40:58 +0530

    assign height for select tag

---
+++ assets/js/crop-management.js
@@ -0,0 +1,6 @@
+const parent4 = document.querySelector("#crop-common-name");
+const z = parent4.children[0];
+
+const heightSize2=z.offsetHeight;
+const parent5 = document.querySelector("#crop-field");
+parent5.style.height = heightSize2 + "px";
