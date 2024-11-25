f5a5b65b5f37ff069bf8a01aab885b85fd47a9cd
ThesanduJ <thesandu1602500@gmail.com>
2024-11-25 13:56:27 +0530

    add staff-management form

---
+++ assets/js/staff-management.js
@@ -0,0 +1,19 @@
+const parent6 = document.querySelector("#joined-date");
+const a = parent6.children[0];
+
+const heightSize3=a.offsetHeight;
+const parent7 = document.querySelector("#gender");
+parent7.style.height = heightSize3 + "px";
+
+const parent8 = document.querySelector("#role");
+parent8.style.height = heightSize3 + "px";
+
+const parent9 = document.querySelector("#staff-field");
+parent9.style.height = heightSize3 + "px";
+
+const parent10 = document.querySelector("#staff-vehicle");
+parent10.style.height = heightSize3 + "px";
+//--------------------------------------------------------------------
+
+
+//--------------------------------------------------------------------
