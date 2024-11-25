fccb33fddc045a91b6f7e4da467bacbf73c4a4b0
ThesanduJ <thesandu1602500@gmail.com>
2024-11-25 15:48:28 +0530

    add log management for

---
+++ assets/js/log-management.js
@@ -0,0 +1,13 @@
+const parent11 = document.querySelector("#observed-image");
+const c = parent11.children[0];
+
+const heightSize4=c.offsetHeight;
+const parent12 = document.querySelector("#log-field");
+parent12.style.height = heightSize4 + "px";
+
+const parent13 = document.querySelector("#log-crop");
+parent13.style.height = heightSize4 + "px";
+
+
+const parent14 = document.querySelector("#log-staff");
+parent14.style.height = heightSize4 + "px";
