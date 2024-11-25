22776a27d64feb2432618eb1d7e9bd57fe18d10b
ThesanduJ <thesandu1602500@gmail.com>
2024-11-25 18:11:43 +0530

    add vehicle management form

---
+++ assets/js/vehicle-management.js
@@ -0,0 +1,6 @@
+const parent15 = document.querySelector("#remarks");
+const d = parent15.children[0];
+
+const heightSize5=d.offsetHeight;
+const parent16 = document.querySelector("#allocated-staff-member-details");
+parent16.style.height = heightSize5 + "px";
