bac4631bb19955f5d3813ac18be80c2723af72f5
ThesanduJ <thesandu1602500@gmail.com>
2024-11-25 20:23:31 +0530

    add equipment management form

---
+++ assets/js/equipment-management.js
@@ -0,0 +1,15 @@
+const parent17 = document.querySelector("#equipment-name");
+const e = parent17.children[0];
+
+const heightSize6=e.offsetHeight;
+const parent18 = document.querySelector("#equipment-type");
+parent18.style.height = heightSize6 + "px";
+
+const parent19 = document.querySelector("#equipment-status");
+parent19.style.height = heightSize6 + "px";
+
+const parent20 = document.querySelector("#equipment-staff");
+parent20.style.height = heightSize6 + "px";
+
+const parent21 = document.querySelector("#equipment-field");
+parent21.style.height = heightSize6 + "px";
